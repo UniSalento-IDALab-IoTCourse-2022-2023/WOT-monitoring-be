dde829737b9db8ed10db587098ece66f60c7f954
Cyber-Vadok <robertovadacca22@gmail.com>
2023-07-14 09:17:03 +0200

    added sec and cors

---
+++ Monitoring-be/src/main/java/it/unisalento/iot/monitoringbe/configurations/CorsConfig.java
@@ -0,0 +1,28 @@
+package it.unisalento.iot.monitoringbe.configurations;
+
+import java.util.List;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+@Configuration
+public class CorsConfig {
+
+	@Bean
+	CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOriginPatterns(List.of("*"));
+		config.addAllowedMethod("*");
+		config.addAllowedHeader("*");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+
+	}
+
+}
